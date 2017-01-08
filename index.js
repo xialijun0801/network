db03e57247350af64647372066565718101b2db8
xiaxia <xialijun0801@gmail.com>
2017-01-07 22:48:10 -0500

    update

--- index.js
+++ index.js
@@ -2,10 +2,19 @@
 var path = require("path");
 var express = require("express");
 var app = express();
+//var morgan = require('morgan')
 
+//app.use(morgan('combined'))
 app.use(express.static(__dirname+'/image'));
+
 app.get("/",function(req,res){
     res.sendFile(path.join(__dirname, "test.html"));
 });
 
-app.listen(80);
+app.listen(80, function(err) {
+   if(err){
+       console.log(err); 
+   } else {
+       console.log("Listening on port 80");  
+   }
+});
