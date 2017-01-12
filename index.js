51ea0511de4f4612dfa5edd54999bd077a6910fd
xiaxia <xialijun0801@gmail.com>
2017-01-12 00:55:12 -0500

    version 1 work

--- index.js
+++ index.js
@@ -10,11 +10,11 @@ var app = express();
 
 //app.use(morgan('combined'))
 app.use(express.static(__dirname+'/image'));
-app.use(express.static(__dirname+'/node_modules/angular/angular.js'));
-
+app.use("/angular", express.static(path.join(__dirname,"node_modules/angular")));
 app.use("/login", express.static(path.join(__dirname, "login")));
 app.use("/register", express.static(path.join(__dirname, "register")));
 app.use("/home", express.static(path.join(__dirname, "home")));
+
 
 /*app.get("/",function(req,res){
     res.sendFile(path.join(__dirname, "test.html"));
